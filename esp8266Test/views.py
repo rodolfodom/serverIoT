a9758e57f66ae2ef92e1f80c5be10e22ef0f50f7
rodolfodom <rodolfodom7@gmail.com>
2023-10-08 22:41:42 -0600

    added decorator in views

--- esp8266Test/views.py
+++ esp8266Test/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_http_methods, require_GET, require_POST, require_http_methods, require_safe
 import json
 
+@require_http_methods(["POST"])
 @csrf_exempt
 def index(request):
     if(request.method == "POST"):
