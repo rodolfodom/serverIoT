2beae3cac0b7484546126e5804b141ca390dbc64
Rodolfo Domínguez <rodolfodominguez60@aragon.unam.mx>
2023-10-11 10:48:24 -0600

    ksidjdsa

--- esp8266Test/views.py
+++ esp8266Test/views.py
@@ -4,11 +4,18 @@ from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods, require_GET, require_POST, require_http_methods, require_safe
 import json
 
+def distancia(adc):
+    distancia_cm = 17569.7 * (adc ** -1.2062)
+    return distancia_cm
+
+
+
 @require_http_methods(["POST"])
 @csrf_exempt
 def index(request):
     if(request.method == "POST"):
         data = json.loads(request.body.decode("utf-8"))
-        value = data.get("value")
-        print(value)
-    return HttpResponse("5")
+        value = int(data.get("value"))
+        distancia_cm = distancia(value)
+        
+    return HttpResponse(distancia_cm)
